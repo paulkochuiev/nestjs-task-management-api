6c7eed88a81cb2d74398588bc55a91916e40d454
paulkochuiev <paulkochuiev@gmail.com>
2026-03-03 22:14:29 +0100

    feat: add database config

--- src/config/config.types.ts
+++ src/config/config.types.ts
@@ -7,4 +7,9 @@ export interface ConfigType {
 
 export const appConfigSchema = Joi.object({
   APP_MESSAGE_PREFIX: Joi.string().default('Tasks '),
+  DB_HOST: Joi.string().default('locahost'),
+  DB_PORT: Joi.number().default(5432),
+  DB_USER: Joi.string().required(),
+  DB_PASSWORD: Joi.string().required(),
+  DB_DATABASE: Joi.string().required(),
 });
